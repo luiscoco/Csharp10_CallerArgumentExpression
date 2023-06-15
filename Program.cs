9e0f633a5c138b5966dd3248b31e70cd4688e0da
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-06-15 22:18:22 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+string someString = "Luis";
+string actualString = someString;
+
+Verify verify = new();
+verify.IsTrue(actualString, someString == "TEST");
+
+public class Verify
+{
+    public void IsTrue(string actualString, bool someString, [CallerArgumentExpression("someString")] string expression = null)
+    {
+        if (!someString)
+        {
+            Console.WriteLine($"{expression} must be True, but was False. The actual string values is: {actualString}");
+        }
+        else 
+        {
+            Console.WriteLine($"Correct the string value is: {expression}");
+        }
+    }
+}
